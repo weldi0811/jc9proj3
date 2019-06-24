53527bb56e68fcc6fd7d02f6b9c6aa18e0e89fc9
weldi0811 <weldi0811@gmail.com>
2019-06-24 20:20:10 +0700

    nambahin login

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,7 +1,45 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import axios from 'axios';
+import Register from './Register'
+import {Redirect} from 'react-router-dom'
 
 class Login extends React.Component{
+
+    onButtonClick = () => {
+        const inputEmail = this.email.value
+        const inputpassword = this.password.value
+
+        if (inputEmail === ''){
+            alert('must fill email')
+        }
+        else if(inputpassword.length < 8){
+            alert('password must be 8 character or more')
+        }
+        else{
+            axios.get('http://localhost:4000/users',{
+                        params:{
+                        email: inputEmail
+                        }
+                    }).then((res) => {
+                        if(res.data.length > 0){
+                            alert('welcome ')
+                        }
+                        else{
+                            var check = window.confirm('email not registered. register now? ')
+                             if (check == true) {
+                            return <Link to='/register'/> //belom bisa
+                             }
+                            else{
+                                window.location.reload()
+                            }
+                        }
+                    })
+
+        }
+        
+    }
+
     render (){
         return (
             <div>
